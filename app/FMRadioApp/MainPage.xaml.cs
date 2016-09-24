bf1f785ae000a4255968cdfe23b9689ed0450ad1
Matthew Leibowitz <mattleibow@live.com>
2016-09-24 02:32:33 +0200

    Setting the frequency is now on another thread to prevent the UI locking  - a few tweaks so that we always get a correct FM frequency (at least for the US)

--- app/FMRadioApp/MainPage.xaml.cs
+++ app/FMRadioApp/MainPage.xaml.cs
@@ -8,6 +8,8 @@ using Microsoft.Devices.Radio;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading;
+using Windows.UI.Core;
 
 namespace FMRadioApp
 {
@@ -21,41 +23,57 @@ namespace FMRadioApp
 				{ RadioRegion.Japan, new Tuple<double, double>(76.0, 95.0) },
 			};
 
-		private FMRadio radio;
-		private RadioRegion? region;
+		private readonly FMRadio radio;
 		private double frequency;
 		private double minFrequency;
 		private double maxFrequency;
+
+		private Task radioUpdateTimer;
 
 		public MainPage()
 		{
 			this.InitializeComponent();
 
+			// regions
 			Regions = new ObservableCollection<RadioRegion>(Enum.GetValues(typeof(RadioRegion)).Cast<RadioRegion>());
 
-			DataContext = this;
-		}
-
-		protected override async void OnNavigatedTo(NavigationEventArgs e)
-		{
-			base.OnNavigatedTo(e);
-
-			// set up the radio
-			if (radio == null)
-			{
-				GetRadio();
-			}
-		}
-
-		private void GetRadio()
-		{
+			// the radio
 			try
 			{
-				Radio = FMRadio.Instance;
+				radio = FMRadio.Instance;
+				Region = radio.CurrentRegion;
+				Frequency = radio.Frequency;
+				RadioOn = radio.PowerMode == RadioPowerMode.On;
 			}
 			catch (RadioDisabledException)
 			{
-				Radio = null;
+				radio = null;
+			}
+			OnPropertyChanged();
+			OnPropertyChanged(nameof(RadioSupported));
+
+			// start binding
+			DataContext = this;
+
+			// start the timer
+			radioUpdateTimer = Task.Run(OnRefreshRadio);
+		}
+
+		private async Task OnRefreshRadio()
+		{
+			while (true)
+			{
+				// we don't want to stop the UI at all
+				if (RadioOn && Math.Abs(radio.Frequency - Frequency) > 0.1)
+				{
+					radio.Frequency = Frequency;
+				}
+
+				// update the signal UI
+				await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => OnPropertyChanged(nameof(SignalStrength)));
+
+				// restart the timer
+				await Task.Delay(500);
 			}
 		}
 
@@ -103,30 +121,17 @@ namespace FMRadioApp
 			get { return radio != null; }
 		}
 
-		public FMRadio Radio
-		{
-			get { return radio; }
-			set
-			{
-				radio = value;
-				Region = radio.CurrentRegion;
-				OnPropertyChanged();
-				OnPropertyChanged(nameof(RadioSupported));
-			}
-		}
-
 		public ObservableCollection<RadioRegion> Regions { get; }
 
 		public RadioRegion Region
 		{
-			get { return region ?? RadioRegion.UnitedStates; }
+			get { return radio?.CurrentRegion ?? RadioRegion.UnitedStates; }
 			set
 			{
-				region = value;
+				radio.CurrentRegion = value;
 
 				MaxFrequency = frequencyLimits[Region].Item2;
 				MinFrequency = frequencyLimits[Region].Item1;
-				OnPropertyChanged(nameof(Frequency));
 
 				OnPropertyChanged();
 			}
@@ -134,7 +139,7 @@ namespace FMRadioApp
 
 		public double SignalStrength
 		{
-			get { return radio?.SignalStrength ?? 0.0; }
+			get { return RadioOn ? radio.SignalStrength : 0.0; }
 		}
 
 		public double MinFrequency
@@ -152,16 +157,24 @@ namespace FMRadioApp
 		public double Frequency
 		{
 			get { return frequency; }
-			set
+			set { frequency = CorrectFrequency(value); OnPropertyChanged(); }
+		}
+
+		private double CorrectFrequency(double value)
+		{
+			// round
+			value = Math.Round(value, 2);
+
+			if (Region == RadioRegion.UnitedStates)
 			{
-				frequency = value;
-				if (RadioOn)
+				// if the decimal is even, then move it one up
+				if ((value * 10) % 2 == 0)
 				{
-					radio.Frequency = frequency;
+					value += 0.1;
 				}
-				OnPropertyChanged();
-				OnPropertyChanged(nameof(SignalStrength));
 			}
+
+			return Math.Round(value, 2);
 		}
 
 		public bool RadioOn
@@ -172,13 +185,8 @@ namespace FMRadioApp
 				if (RadioSupported)
 				{
 					radio.PowerMode = value ? RadioPowerMode.On : RadioPowerMode.Off;
-					if (value)
-					{
-						radio.Frequency = frequency;
-					}
+					OnPropertyChanged();
 				}
-				OnPropertyChanged();
-				OnPropertyChanged(nameof(SignalStrength));
 			}
 		}
 
